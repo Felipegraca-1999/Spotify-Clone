d44ca998d98fe5c0daa56b50c102b473fb036aed
unknown <felipegracaa.1999@gmail.com>
2022-03-23 14:30:41 -0300

    Criação das funções de alteração dos icones

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -7,9 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+
+  ativacaoEmbaralhamento: boolean = false;
+  ativacaoRepeticao: boolean = false;
+  ativacaoPausePlay: boolean = false;
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+
+  //ativando bolinhas de embaralhamento
+  embaralhamento() {
+    if (!this.ativacaoEmbaralhamento) {
+      var element = document.getElementById('embaralhamento');
+      element?.classList.add('bolinha');
+    } else {
+      var element = document.getElementById('embaralhamento');
+      element?.classList.remove('bolinha');
+    }
+    this.ativacaoEmbaralhamento = !this.ativacaoEmbaralhamento;
+  }
+
+  //ativando bolinhas de repetição
+  repeticao() {
+    if (!this.ativacaoRepeticao) {
+      var element = document.getElementById('repeticao');
+      element?.classList.add('bolinha');
+    } else {
+      var element = document.getElementById('repeticao');
+      element?.classList.remove('bolinha');
+    }
+    this.ativacaoRepeticao = !this.ativacaoRepeticao;
+  }
+
+  // Alterando icone de play e pause quando clicado
+  pausePlay() {
+    if (!this.ativacaoPausePlay) {
+      var element1 = document.getElementById('pauseIcon');
+      var element2 = document.getElementById('playIcon');
+
+      element1?.classList.add('displayOff');
+      element1?.classList.remove('displayOn');
+
+      element2?.classList.add('displayOn');
+      element2?.classList.remove('displayOff');
+
+    } else {
+
+      var element1 = document.getElementById('pauseIcon');
+      var element2 = document.getElementById('playIcon');
+
+      element1?.classList.add('displayOn');
+      element1?.classList.remove('displayOff');
+
+      element2?.classList.add('displayOff');
+      element2?.classList.remove('displayOn');
+    }
+    this.ativacaoPausePlay = !this.ativacaoPausePlay;
   }
 
 }
